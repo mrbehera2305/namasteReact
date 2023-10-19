617193ba19a6777eb1c4bdbba908374e7e0bdcf4
Manoranjan <mrbehera2305@gmail.com>
2023-10-19 15:49:58 +0530

    adding parcel

---
+++ dist/index.6bd02f5a.js
@@ -0,0 +1,4 @@
+let head = document.getElementById("head");
+head.innerText = "Welcome to Namste React";
+
+//# sourceMappingURL=index.6bd02f5a.js.map
---
+++ scr/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Headers from "./components/Headers";
+
+const App = () => {
+  return (
+    <>
+      <Headers />
+    </>
+  );
+};
+
+export default App;
